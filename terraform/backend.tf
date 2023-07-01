23be89e809df03305296c5c42813f9df6cbd842c
Ilkcan Keles <cankelesilk@gmail.com>
2023-07-01 21:13:31 +0000

    terraform format

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # CoRise TODO: replace with the bucket name you created
-    bucket = "terraform-state-flaskbb-ilkcan" 
+    bucket = "terraform-state-flaskbb-ilkcan"
     key    = "core/terraform.tfstate"
     # CoRise TODO: replace with the region you are using
     region = "eu-north-1"
